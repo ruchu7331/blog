15b93242de53de626b6ce16814a9b3f78becc65a
ruchupang <1043153801@qq.com>
2021-01-13 10:02:53 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-// import ClassComponent from './pages/ClassComponent'
-// import FunctionComponent from './pages/FunctionComponent'
-// import SetStatePage from './pages/SetStatePage'
-// import UserPage from './pages/UserPage'
-// import HomePage from './pages/HomePage'
-// import SlotPage from './pages/SlotPage'
-// import StorePage from './pages/StorePage'
-// import ReactReduxPage from "./pages/ReactReduxPage";
-// import ReactRouterPage from "./pages/ReactRouterPage";
-// import PurComponentPage from "./pages/PurComponentPage";
-// import HookPage from "./Hook/HookPage";
-// import DiyHookPage from "./Hook/DiyHookPage";
-// import UseMemoPage from "./Hook/UseMemoPage";
-// import UseCallBackPage from "./Hook/UseCallBackPage";
-// import DialogComponentPage from './ComponentDiyHoc/DialogComponentPage'
-import FirstHomePage from './FirstPage/FirstHomePage'
+import React from 'react'
 function App() {
   return (
     <div>
-      {/*
-        <ClassComponent></ClassComponent>
-        <FunctionComponent></FunctionComponent>
-        <SetStatePage></SetStatePage> 
-        <UserPage></UserPage>
-        <HomePage></HomePage>
-        <SlotPage></SlotPage>
-        <StorePage></StorePage>
-        <ReactReduxPage></ReactReduxPage>
-        <ReactRouterPage></ReactRouterPage>
-        <PurComponentPage></PurComponentPage>
-        <HookPage></HookPage>
-        <DiyHookPage></DiyHookPage>
-        <UseMemoPage></UseMemoPage>
-        <UseCallBackPage></UseCallBackPage>
-        <DialogComponentPage></DialogComponentPage>
-      */}
-      <FirstHomePage></FirstHomePage>
+      <h1>1111</h1>
     </div>
   );
 }
